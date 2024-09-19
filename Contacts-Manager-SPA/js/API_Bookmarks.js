2736be350aa666e9148e7e0a5433468d0321bb2c
Jack-Jack <99698713+JackijackDarth@users.noreply.github.com>
2024-09-19 16:34:44 -0400

    ajout des fichiers client

---
+++ Contacts-Manager-SPA/js/API_Bookmarks.js
@@ -0,0 +1,42 @@
+const API_URL = "https://evanescent-brassy-quail.glitch.me/api/bookmarks";
+function API_GetBookmarks() {
+    return new Promise(resolve => {
+        $.ajax({
+            url: API_URL,
+            success: bookmarks => { resolve(bookmarks); },
+            error: (xhr) => { console.log(xhr); resolve(null); }
+        });
+    });
+}
+function API_GetBookmark(bookmarkId) {
+    return new Promise(resolve => {
+        $.ajax({
+            url: API_URL + "/" + bookmarkId,
+            success: bookmark => { resolve(bookmark); },
+            error: () => { resolve(null); }
+        });
+    });
+}
+function API_SaveBookmark(bookmark, create) {
+    console.log("Saving bookmark:", bookmark);  // Add this to inspect the bookmark object
+    return new Promise(resolve => {
+        $.ajax({
+            url: API_URL + (create ? "" : "/" + bookmark.Id),
+            type: create ? "POST" : "PUT",
+            contentType: 'application/json',
+            data: JSON.stringify(bookmark),
+            success: (/*data*/) => { resolve(true); },
+            error: (/*xhr*/) => { resolve(false /*xhr.status*/); }
+        });
+    });
+}
+function API_DeleteBookmark(id) {
+    return new Promise(resolve => {
+        $.ajax({
+            url: API_URL + "/" + id,
+            type: "DELETE",
+            success: () => { resolve(true); },
+            error: (/*xhr*/) => { resolve(false /*xhr.status*/); }
+        });
+    });
+}
